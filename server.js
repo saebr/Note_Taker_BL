d6a32ed9e8b394805f44599680dbf710e8dfba96
Saebr <bensaebr@gmail.com>
2021-04-21 19:51:55 -0500

    Added save functionallity

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const data = require('./db/db.json')
 
 const app = express();
 const PORT = 3001
@@ -9,11 +11,35 @@ app.use(express.json());
 app.use(express.static('public'))
 
 // Sets up basic routes for user
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
+app.get('/api/notes', (req, res) => {
+    return res.json(data)
+});
 
+app.post('/api/notes', (req, res) => {
+    const newData = data.push(req.body)
+    fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+    if (err) throw err
+    })
+    return res.json(newData)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    var id = req.params.id
+    var newData = data.filter(item => {
+        console.log(item.id)
+        console.log(id)
+        return item.id != JSON.parse(id)
+    })
+    console.log(newData)
+    fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+    if (err) throw err
+    })
+    return res.json(id)
+}) 
 
 
 
